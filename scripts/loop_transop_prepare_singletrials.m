7079eedb8601d0c71ceed98054010ea465c0c67a
LaraWie <LaraWie@users.noreply.github.com>
2021-02-09 16:36:05 +0100

    Cleaned hierarchical script mess and added RT

--- scripts/loop_transop_prepare_singletrials.m
+++ scripts/loop_transop_prepare_singletrials.m
@@ -8,7 +8,7 @@ clear
 close all
 
 %%%%%%%%%%%%
-data_path = '/Users/larawieland/Documents/ECN/Promotion/SALAD/data_raw/sample1_operant/CT/'; 
+data_path = '/Users/larawieland/Documents/Promotion/SALAD_3/data_raw/sample1_operant/CT/'; 
 data =  dir([data_path '*.mat']);
 %%%%%%%%%%%%%%
 %% Set history of previous trials and total trials
@@ -17,21 +17,24 @@ ntrials = 160;
 
 
 % initialize variables
-response  = NaN(ntrials, length(data));
-outcome   = NaN(ntrials, length(data));
-winstay   = NaN(ntrials, history, length(data));
-loseshift = NaN(ntrials, history, length(data));
-trial_idx = 1:ntrials;
+response        = NaN(ntrials, length(data));
+outcome         = NaN(ntrials, length(data));
+winstay         = NaN(ntrials, history, length(data));
+loseshift       = NaN(ntrials, history, length(data));
+reaction_times  = NaN(ntrials, length(data));
+
+trial_idx       = 1:ntrials;
 
 
 for sub = 1:length(data)
     sub
     subjects{sub} = data(sub).name(1:end-4);
-    load([data_path data(sub).name], 'A', 'R');
+    load([data_path data(sub).name], 'A', 'R','RT');
     group{sub} = data(sub).name(7); 
     fin        = length(A);
     response(1:fin, sub)   = double(A==1);
     outcome(1:fin, sub) = double(R==1);
+    reaction_times(1:fin, sub) = double(RT);
     
     R     = double(R==1);
     resp  = A;
@@ -57,7 +60,7 @@ sub_name(substart:subend)  = subjects(sub);
 sub_group(1:ntrials) = str2double(group{sub});
 sub_idx(1:ntrials)   = sub;
 
-table(substart:subend,:) = [sub_idx', sub_group', trial_idx', outcome(:,sub), response(:,sub), winstay(:,:,sub), loseshift(:,:,sub)];
+table(substart:subend,:) = [sub_idx', sub_group', trial_idx', outcome(:,sub), response(:,sub), winstay(:,:,sub), loseshift(:,:,sub), reaction_times(:,sub)];
 
     
 end
@@ -67,14 +70,14 @@ n_win_st{h}     = ['winstay_t', num2str(h)];
 n_lose_sh{h}    = ['loseshift_t', num2str(h)];
 end
 
-var_names =['sub_idx', 'Group', 'Trial_idx', 'Outcome', 'Choice_t', n_win_st, n_lose_sh];
+var_names =['sub_idx', 'Group', 'Trial_idx', 'Outcome', 'Choice_t', n_win_st, n_lose_sh, 'RT'];
 
 
 
 
 singletrialstable = array2table(table,'VariableNames',var_names)
 % singletrialstable.Properties.RowNames = sub_name;
-writetable(singletrialstable,'SALAD_CT_TransOp_singletrials.csv','WriteRowNames',true, 'Delimiter', 'comma') 
+writetable(singletrialstable,'SALAD_CT_TransOp_singletrials_wRT.csv','WriteRowNames',true, 'Delimiter', 'comma') 
 
  
 
---
+++ scripts/loop_transop_prepare_singletrials_onetimepoint.m
@@ -0,0 +1,86 @@
+%%%% Loop for extraction of single-trial predictors and dependent variable for winstay/loseshift analysis of reversal learning data
+%%%% in line with den Ouden et al., Neuron, 2013 (analysis from the Supplement)
+%%%% code by Teresa Katthagen, May 2020.
+
+
+clc
+clear
+close all
+
+%%%%%%%%%%%%
+data_path = '/Users/larawieland/Documents/Promotion/SALAD_3/data_raw/sample1_operant/CT/'; 
+data =  dir([data_path '*.mat']);
+%%%%%%%%%%%%%%
+%% Set history of previous trials and total trials
+%history = 4; 
+ntrials = 160;
+
+
+% initialize variables
+response        = NaN(ntrials, length(data));
+outcome         = NaN(ntrials, length(data));
+correct         = NaN(ntrials, length(data));
+winstay         = NaN(ntrials, length(data));
+loseshift       = NaN(ntrials, length(data));
+reaction_times  = NaN(ntrials, length(data));
+
+trial_idx       = 1:ntrials;
+
+
+for sub = 1:length(data)
+    sub
+    subjects{sub} = data(sub).name(1:end-4);
+    load([data_path data(sub).name], 'A', 'R', 'C','RT');
+    group{sub} = data(sub).name(7); 
+    fin        = length(A);
+    response(1:fin, sub)   = double(A==1);
+    outcome(1:fin, sub) = double(R==1);
+    correct(1:fin, sub) = double(C==1);
+    reaction_times(1:fin, sub) = double(RT);
+    
+    R     = double(R==1);
+    resp  = A;
+    resp(resp==2) = -1;
+
+
+    for i = 1:length(R)
+    %for h = 1:history
+    %if i > h
+    win                  = [resp(i), R(i)];
+    winstay(i, sub)   = prod(win);
+    
+    loss                 = [-resp(i), 1-R(i)];
+    loseshift(i, sub)   = prod(loss);
+    %end
+    %end
+    end
+    
+substart             = sub*ntrials - ntrials+1;
+subend               = sub*ntrials;
+
+sub_name(substart:subend)  = subjects(sub);
+sub_group(1:ntrials) = str2double(group{sub});
+sub_idx(1:ntrials)   = sub;
+
+table(substart:subend,:) = [sub_idx', sub_group', trial_idx', outcome(:,sub), response(:,sub), correct(:,sub), winstay(:,sub), loseshift(:,sub), reaction_times(:,sub)];
+
+    
+end
+
+% for h = 1:history
+% n_win_st{h}     = ['winstay_t', num2str(h)];
+% %n_lose_sh{h}    = ['loseshift_t', num2str(h)];
+% end
+
+var_names ={'sub_idx', 'Group', 'Trial_idx', 'Outcome', 'Choice_t','Correct', 'win_st','lose_sh', 'RT'};
+
+
+
+
+singletrialstable = array2table(table,'VariableNames',var_names);
+%singletrialstable.Properties.RowNames = sub_name;
+writetable(singletrialstable,'SALAD_CT_TransOp_singletrials_wRT.csv','WriteRowNames',true, 'Delimiter', 'comma');
+
+ 
+
+
