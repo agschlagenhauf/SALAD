cccea3c05f67d31bebea07c6fc21cb1e326334cc
LaraWie <lara_wieland@hotmail.de>
2021-06-09 15:47:26 +0000

    Added correct reversal sensitivity trajectory

--- preps/rev_sens_singletrial.R
+++ preps/rev_sens_singletrial.R
@@ -13,8 +13,8 @@ se <- function(x) sd(x, na.rm=TRUE)/sqrt(length(x)-sum(is.nan(x)))
 df <- filter(data_prep,Trial_idx %in% c(51:60,121:130))
 
 # create index if first or second reversal
-df$reversal <- 'last'
-df$reversal[df$Trial_idx>=50 & df$Trial_idx<=60] <- 'first'
+df$reversal <- 'Last Reversal'
+df$reversal[df$Trial_idx>=50 & df$Trial_idx<=60] <- 'First Reversal'
 df$reversal <- factor(df$reversal)
 # index number of trials within reversal
 df$revidx <- rep(c(-5:-1,1:5),times=4*28)
@@ -50,8 +50,20 @@ dfsenscort <- merge.data.frame(longdat.cond.both, dfsenswideboth, by = c("just_c
 # preparation for plot, calculate trialwise mean of correct choice across subject
 dfagg <- df %>% group_by(Trial_idx,Cond,revidx,reversal) %>% summarise(corr=mean(Correct,na.rm=TRUE),SE=se(Correct)) 
 # and plot (according to Fig 4, Cremer, 2021)
-revsensplot <- ggplot(dfagg,aes(x = revidx,y = corr,color=Cond)) + geom_point() + geom_line() + geom_errorbar(aes(ymin=corr-SE, ymax=corr+SE),width=.2) + facet_wrap(.~reversal)
+
+dfagg$Cond <- factor(dfagg$Cond, levels=rev(levels(dfagg$Cond)))
+
+revsensplot <- ggplot(dfagg,aes(x = revidx,y = corr,color=Cond))+ 
+                geom_point() + 
+                geom_line() + 
+                geom_errorbar(aes(ymin=corr-SE, ymax=corr+SE),width=.2) + 
+                facet_wrap(.~reversal) +
+  labs(title = '',  x = "Trial relative to reversal", y = "Mean advantageous choices", color = "Condition") 
+
 revsensplot
+
+ggsave(revsensplot, filename = "senscortplotfirst","jpg", "/cloud/project/plots/meeting_plots")
+
 
 # scatter plot for sensitivity index (first or last) with cortisol AUC 
 
@@ -61,15 +73,12 @@ senscortplotfirst <- ggscatter(dfsenscort, x = "aucg", y = "revidx_first",
                               xlab = "AUCG", ylab = "Reversal Sensitivity Index (first)")
 senscortplotfirst
 
-ggsave(senscortplotfirst, filename = "senscortplotfirst","jpg", "/cloud/project/plots/meeting_plots")
 
 senscortplotlast <- ggscatter(dfsenscort, x = "aucg", y = "revidx_last",
                           add = "reg.line", conf.int = TRUE, 
                           cor.coef = TRUE, cor.method = "pearson",
                           xlab = "AUCG", ylab = "Reversal Sensitivity Index (last)")
 senscortplotlast
-
-ggsave(senscortplotlast, filename = "senscortplotlast","jpg", "/cloud/project/plots/meeting_plots")
 
 
 # and bar plot 
@@ -81,7 +90,7 @@ revsensbar <- ggplot(dfsenswideagg,aes(x=Cond, y = mean, color = Cond)) +
               scale_color_manual(values = c("#00BFC4", "#F8766D"))
 revsensbar
 
-#ggsave(revsensbar, filename = "revsensbar","jpg", "/cloud/project/plots/meeting_plots")
+#ggsave(revsensbar, filename = "revsensbar",".jpg", "/cloud/project/plots/meeting_plots")
 
 
 meandifffirst.ttest <- t.test(data=dfsenswidefirst, sensidx ~ Cond, paired = TRUE)
